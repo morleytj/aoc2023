43a007f5a753c621e521c4ae2fac84f363984d89
Theodore Morley <theodore.j.morley@vumc.org>
2023-12-03 15:31:48 -0500

    Day2 start

---
+++ day2.py
@@ -0,0 +1,15 @@
+import sys
+
+'''
+Input:
+    line: a line of format "Game N: comma separated values by color, semicolon separating each trial
+    red_l: limit of reds to check
+    green_l: limit of greens
+    blue_l: limit of blues
+'''
+def check_validity(line,red_l,green_l,blue_l):
+    #
+
+
+if __name__=="__main__":
+    f = open(sys.argv[1],'r')
